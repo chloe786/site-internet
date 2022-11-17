f5d5cf9552ba54862f3f61312a62b07ee9d6fe67
chloe786 <115727703+chloe786@users.noreply.github.com>
2022-11-17 17:36:33 +0100

    Update app.js

--- PROJET/app.js
+++ PROJET/app.js
@@ -1,22 +1,22 @@
-/*navbar couleur scroll*/
+/*navbar couleur scroll blanc*/
 window.addEventListener('scroll', function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
-/*lier onglet avec page*/
+/*lier onglets avec pages*/
 
-function toggleVilla(){
+function toggleVilla(){    /*toggle = basculer*/
     const toggleVilla = document.querySelector('.villatoggle');
     const navbar = document.querySelector('navbar');
     villatoggle.classList.toggle('active');
-    navbar.classList.toggle('active');
+    navbar.classList.toggle('active');    /*classList =chaine composée de la liste des classes*/
 }
 
  
 /*navbar responsive hamburger*/
 
-const menuHamburger = document.querySelector(".menu-hamburger")
+const menuHamburger = document.querySelector(".menu-hamburger")  /*querySelector = retourne le premier Element dans le document correspondant au sélecteur*/
 const navbar = document.querySelector(".navbar")
 
 menuHamburger.addEventListener('click',()=>{
